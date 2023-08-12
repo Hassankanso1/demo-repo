33c51b0281273ebbe3947c2a40a75ad4b42ac1d5
Hassankanso1 <hassankanso1@outlook.com>
2023-08-12 13:24:21 +0300

    new

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,9 @@
-
+def reverse(text,i)
+    reversed=""
+    for index in range(1,len(text),+1):
+        reversed+=(len(text)-1)
+    result=""
+    for index in range(i):
+        result+=reversed
+    print(result)
+reverse("kanso",2)
